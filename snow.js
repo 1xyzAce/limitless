a9b8baa7bb2d368bc41eda0a9e608056058928e4
tmp <168496015+1xyzAce@users.noreply.github.com>
2024-07-16 14:45:01 +0100

    Delete snow.js

--- snow.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    if (document.body.classList.contains('snow')) {
-        const snowflakes = document.querySelectorAll('.snowflake');
-        snowflakes.forEach(snowflake => {
-            snowflake.style.animation = 'fall 10s linear infinite';
-        });
-    }
-});
